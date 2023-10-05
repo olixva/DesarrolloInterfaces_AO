a50b18d8eff616395ee805635f487ba3f74b939e
Antonio Oliva Carceles <116766832+olixva@users.noreply.github.com>
2023-10-06 00:02:50 +0200

    Hoja 2 Ejercicios terminados a falta de repasar

--- hoja2EjerciciosC#/Form1.Designer.cs
+++ hoja2EjerciciosC#/Form1.Designer.cs
@@ -37,6 +37,10 @@
             this.btnBuscar = new System.Windows.Forms.Button();
             this.lblEsPrimo = new System.Windows.Forms.Label();
             this.lblNPrimo = new System.Windows.Forms.Label();
+            this.lblRepite = new System.Windows.Forms.Label();
+            this.lblMedia = new System.Windows.Forms.Label();
+            this.lblMayor = new System.Windows.Forms.Label();
+            this.lblMenor = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // listBoxNumeros
@@ -46,7 +50,7 @@
             this.listBoxNumeros.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.listBoxNumeros.FormattingEnabled = true;
             this.listBoxNumeros.ItemHeight = 38;
-            this.listBoxNumeros.Location = new System.Drawing.Point(428, 95);
+            this.listBoxNumeros.Location = new System.Drawing.Point(436, 96);
             this.listBoxNumeros.Name = "listBoxNumeros";
             this.listBoxNumeros.Size = new System.Drawing.Size(120, 498);
             this.listBoxNumeros.TabIndex = 0;
@@ -54,7 +58,7 @@
             // btnOrdCacl
             // 
             this.btnOrdCacl.Enabled = false;
-            this.btnOrdCacl.Location = new System.Drawing.Point(106, 212);
+            this.btnOrdCacl.Location = new System.Drawing.Point(124, 212);
             this.btnOrdCacl.Name = "btnOrdCacl";
             this.btnOrdCacl.Size = new System.Drawing.Size(173, 61);
             this.btnOrdCacl.TabIndex = 1;
@@ -64,7 +68,7 @@
             // 
             // txtIntroNum
             // 
-            this.txtIntroNum.Location = new System.Drawing.Point(148, 158);
+            this.txtIntroNum.Location = new System.Drawing.Point(162, 158);
             this.txtIntroNum.Name = "txtIntroNum";
             this.txtIntroNum.Size = new System.Drawing.Size(100, 28);
             this.txtIntroNum.TabIndex = 2;
@@ -73,7 +77,7 @@
             // lblIntroNum
             // 
             this.lblIntroNum.AutoSize = true;
-            this.lblIntroNum.Location = new System.Drawing.Point(82, 116);
+            this.lblIntroNum.Location = new System.Drawing.Point(92, 116);
             this.lblIntroNum.Name = "lblIntroNum";
             this.lblIntroNum.Size = new System.Drawing.Size(251, 22);
             this.lblIntroNum.TabIndex = 3;
@@ -82,7 +86,7 @@
             // txtBuscar
             // 
             this.txtBuscar.Enabled = false;
-            this.txtBuscar.Location = new System.Drawing.Point(742, 158);
+            this.txtBuscar.Location = new System.Drawing.Point(754, 158);
             this.txtBuscar.Name = "txtBuscar";
             this.txtBuscar.Size = new System.Drawing.Size(100, 28);
             this.txtBuscar.TabIndex = 4;
@@ -90,7 +94,7 @@
             // lblBuscar
             // 
             this.lblBuscar.AutoSize = true;
-            this.lblBuscar.Location = new System.Drawing.Point(678, 116);
+            this.lblBuscar.Location = new System.Drawing.Point(690, 116);
             this.lblBuscar.Name = "lblBuscar";
             this.lblBuscar.Size = new System.Drawing.Size(228, 22);
             this.lblBuscar.TabIndex = 5;
@@ -99,21 +103,21 @@
             // btnBuscar
             // 
             this.btnBuscar.Enabled = false;
-            this.btnBuscar.Location = new System.Drawing.Point(742, 212);
+            this.btnBuscar.Location = new System.Drawing.Point(754, 212);
             this.btnBuscar.Name = "btnBuscar";
             this.btnBuscar.Size = new System.Drawing.Size(100, 61);
             this.btnBuscar.TabIndex = 6;
-            this.btnBuscar.Text = "Calcular";
+            this.btnBuscar.Text = "Buscar";
             this.btnBuscar.UseVisualStyleBackColor = true;
             this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
             // 
             // lblEsPrimo
             // 
             this.lblEsPrimo.AutoSize = true;
-            this.lblEsPrimo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblEsPrimo.Location = new System.Drawing.Point(79, 513);
+            this.lblEsPrimo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEsPrimo.Location = new System.Drawing.Point(118, 294);
             this.lblEsPrimo.Name = "lblEsPrimo";
-            this.lblEsPrimo.Size = new System.Drawing.Size(154, 38);
+            this.lblEsPrimo.Size = new System.Drawing.Size(128, 31);
             this.lblEsPrimo.TabIndex = 7;
             this.lblEsPrimo.Text = "es Primo";
             this.lblEsPrimo.Visible = false;
@@ -121,20 +125,68 @@
             // lblNPrimo
             // 
             this.lblNPrimo.AutoSize = true;
-            this.lblNPrimo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblNPrimo.Location = new System.Drawing.Point(28, 513);
+            this.lblNPrimo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNPrimo.Location = new System.Drawing.Point(54, 294);
             this.lblNPrimo.Name = "lblNPrimo";
-            this.lblNPrimo.Size = new System.Drawing.Size(35, 38);
+            this.lblNPrimo.Size = new System.Drawing.Size(28, 31);
             this.lblNPrimo.TabIndex = 8;
             this.lblNPrimo.Text = "x";
             this.lblNPrimo.Visible = false;
+            // 
+            // lblRepite
+            // 
+            this.lblRepite.AutoSize = true;
+            this.lblRepite.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRepite.Location = new System.Drawing.Point(583, 304);
+            this.lblRepite.Name = "lblRepite";
+            this.lblRepite.Size = new System.Drawing.Size(395, 31);
+            this.lblRepite.TabIndex = 9;
+            this.lblRepite.Text = "El numero x se repite x veces";
+            this.lblRepite.Visible = false;
+            // 
+            // lblMedia
+            // 
+            this.lblMedia.AutoSize = true;
+            this.lblMedia.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMedia.Location = new System.Drawing.Point(44, 371);
+            this.lblMedia.Name = "lblMedia";
+            this.lblMedia.Size = new System.Drawing.Size(202, 31);
+            this.lblMedia.TabIndex = 10;
+            this.lblMedia.Text = "La media es: x";
+            this.lblMedia.Visible = false;
+            // 
+            // lblMayor
+            // 
+            this.lblMayor.AutoSize = true;
+            this.lblMayor.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMayor.Location = new System.Drawing.Point(40, 427);
+            this.lblMayor.Name = "lblMayor";
+            this.lblMayor.Size = new System.Drawing.Size(303, 31);
+            this.lblMayor.TabIndex = 11;
+            this.lblMayor.Text = "El numero mayor es: x";
+            this.lblMayor.Visible = false;
+            // 
+            // lblMenor
+            // 
+            this.lblMenor.AutoSize = true;
+            this.lblMenor.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMenor.Location = new System.Drawing.Point(39, 487);
+            this.lblMenor.Name = "lblMenor";
+            this.lblMenor.Size = new System.Drawing.Size(304, 31);
+            this.lblMenor.TabIndex = 13;
+            this.lblMenor.Text = "El numero menor es: x";
+            this.lblMenor.Visible = false;
             // 
             // Ejercicio1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightSeaGreen;
-            this.ClientSize = new System.Drawing.Size(1010, 648);
+            this.ClientSize = new System.Drawing.Size(1042, 648);
+            this.Controls.Add(this.lblMenor);
+            this.Controls.Add(this.lblMayor);
+            this.Controls.Add(this.lblMedia);
+            this.Controls.Add(this.lblRepite);
             this.Controls.Add(this.lblNPrimo);
             this.Controls.Add(this.lblEsPrimo);
             this.Controls.Add(this.btnBuscar);
@@ -162,6 +214,10 @@
         private System.Windows.Forms.Button btnBuscar;
         private System.Windows.Forms.Label lblEsPrimo;
         private System.Windows.Forms.Label lblNPrimo;
+        private System.Windows.Forms.Label lblRepite;
+        private System.Windows.Forms.Label lblMedia;
+        private System.Windows.Forms.Label lblMayor;
+        private System.Windows.Forms.Label lblMenor;
     }
 }
 
--- hoja2EjerciciosC#/Form1.cs
+++ hoja2EjerciciosC#/Form1.cs
@@ -23,19 +23,20 @@ namespace hoja2EjerciciosC_
                 txtIntroNum.Text = "";
 
 
-                if (esPrimo(n))
+                if (EsPrimo(n))
                 {
-                    lblEsPrimo.Text = n.ToString() + " es Primo";
+                    lblEsPrimo.Text = $"{n} es Primo";
                 }
                 else
                 {
-                    lblEsPrimo.Text = n.ToString() + " no es Primo";
+                    lblEsPrimo.Text = $"{n} no es Primo";
                 }
                 lblEsPrimo.Visible = true;
 
                 if (indice == 10)
                 {
                     txtIntroNum.Enabled = false;
+                    txtBuscar.Enabled = true;
                     btnOrdCacl.Enabled = true;
                     btnBuscar.Enabled = true;
                 }
@@ -44,15 +45,46 @@ namespace hoja2EjerciciosC_
 
         private void btnOrdCacl_Click(object sender, EventArgs e)
         {
+            float media = 0f;
 
+            lblNPrimo.Visible = false;
+            lblEsPrimo.Visible = false;
+
+            Ordenar(numeros);
+            listBoxNumeros.Items.Clear();
+            foreach (int n in numeros)
+            {
+                listBoxNumeros.Items.Add(n);
+                media += n;
+            }
+            media /= 10;
+            lblMayor.Text = $"El numero mayor es {numeros[9]}";
+            lblMenor.Text = $"El numero menor es {numeros[0]}";
+            lblMedia.Text = $"La media es {media}";
+            lblMayor.Visible = true;
+            lblMenor.Visible = true;
+            lblMedia.Visible = true;
         }
 
         private void btnBuscar_Click(object sender, EventArgs e)
         {
+            if (Int32.TryParse(txtBuscar.Text, out int buscar))
+            {
+                int i = 0;
+                foreach (int n in numeros)
+                {
+                    if (n == buscar)
+                    {
+                        i++;
+                    }
+                }
+                lblRepite.Text = $"El numero {buscar} se repite {i} veces";
+                lblRepite.Visible = true;
+            }
 
         }
 
-        private bool esPrimo(int n)
+        private bool EsPrimo(int n)
         {
             if (n <= 1)
             {
@@ -67,6 +99,23 @@ namespace hoja2EjerciciosC_
                 }
             }
             return true;
+        }
+
+        private void Ordenar(int[] array)
+        {
+            int n = array.Length;
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        int temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                    }
+                }
+            }
         }
     }
 }
