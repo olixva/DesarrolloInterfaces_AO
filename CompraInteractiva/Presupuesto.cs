8782e2e2e595afe647e377beb617e2e11397a6e4
Antonio Oliva Carceles <116766832+olixva@users.noreply.github.com>
2023-11-23 19:06:05 +0100

    Version final, facturas terminadas

--- CompraInteractiva/Presupuesto.cs
+++ CompraInteractiva/Presupuesto.cs
@@ -9,14 +9,54 @@ namespace CompraInteractiva
         public Presupuesto(List<String> pedido, String metodoPago, int total)
         {
             InitializeComponent();
+
+            richTextBox.SelectionAlignment = HorizontalAlignment.Center;
+            richTextBox.Text += "------------------------------------------\n";
+            richTextBox.Text += "Factura de Compra           \n";
+            richTextBox.Text += "------------------------------------------\n\n";
+
+            richTextBox.SelectionAlignment = HorizontalAlignment.Left;
+            richTextBox.Text += $"{PadRight("Descripción", 40)} {PadLeft("Precio", 10)}\n";
+            richTextBox.Text += "------------------------------------------\n";
+
             foreach (var item in pedido)
             {
-                richTextBox.Text += item + "\n";
+                if (item != null)
+                {
+                    string[] parts = item.Split(new[] { " (" }, StringSplitOptions.RemoveEmptyEntries);
+
+                    if (parts.Length == 2)
+                    {
+                        string nombre = parts[0];
+                        string precio = parts[1].Trim(')');
+                        richTextBox.Text += $"{PadRight(nombre, 40)} {PadLeft(precio, 10)}\n";
+                    }
+                    else
+                    {
+                        richTextBox.Text += $"{item}\n";
+                    }
+                }
+
             }
-            richTextBox.Text += "\n\n\nSubotal = " + (total * 0.79) + "$";
-            richTextBox.Text += "\nIVA (21%) = " + (total * 0.21) + "$";
-            richTextBox.Text += "\nTotal = " + total + "$";
-            richTextBox.Text += "\n\nMetodo de pago = " + metodoPago;
+
+            richTextBox.Text += "------------------------------------------\n";
+            richTextBox.Text += $"{PadRight("Subtotal", 40)} {PadLeft($"{total * 0.79:0.00}$", 10)}\n";
+            richTextBox.Text += $"{PadRight("IVA (21%)", 40)} {PadLeft($"{total * 0.21:0.00}$", 10)}\n";
+            richTextBox.Text += $"{PadRight("Total", 40)} {PadLeft($"{total:0.00}$", 10)}\n";
+            richTextBox.Text += "------------------------------------------\n\n";
+
+            richTextBox.SelectionAlignment = HorizontalAlignment.Center;
+            richTextBox.Text += $"Método de pago: {metodoPago}\n";
+        }
+
+        private string PadRight(string str, int length)
+        {
+            return str.PadRight(length - (length - str.Length) / 2);
+        }
+
+        private string PadLeft(string str, int length)
+        {
+            return str.PadLeft(length - (length - str.Length) / 2);
         }
     }
 }
