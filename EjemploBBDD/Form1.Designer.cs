bfbb9e66b6e7c28bd2a95a8f1633193b595b6c7a
Antonio Oliva Carceles <116766832+olixva@users.noreply.github.com>
2024-01-23 12:35:44 +0100

    Clase 23/01 Ejemplo conexion bbdd a medio

--- EjemploBBDD/Form1.Designer.cs
+++ EjemploBBDD/Form1.Designer.cs
@@ -28,13 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.btnCargarDatos = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowHeadersWidth = 67;
+            this.dataGridView.RowTemplate.Height = 30;
+            this.dataGridView.Size = new System.Drawing.Size(776, 426);
+            this.dataGridView.TabIndex = 0;
+            // 
+            // btnCargarDatos
+            // 
+            this.btnCargarDatos.Location = new System.Drawing.Point(69, 522);
+            this.btnCargarDatos.Name = "btnCargarDatos";
+            this.btnCargarDatos.Size = new System.Drawing.Size(153, 87);
+            this.btnCargarDatos.TabIndex = 1;
+            this.btnCargarDatos.Text = "CARGAR DATOS";
+            this.btnCargarDatos.UseVisualStyleBackColor = true;
+            this.btnCargarDatos.Click += new System.EventHandler(this.btnCargarDatos_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 685);
+            this.Controls.Add(this.btnCargarDatos);
+            this.Controls.Add(this.dataGridView);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Button btnCargarDatos;
     }
 }
 
--- EjemploBBDD/Form1.cs
+++ EjemploBBDD/Form1.cs
@@ -1,22 +1,99 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 using System.Data.SqlClient;
+using System.Windows.Forms;
 
 
 namespace EjemploBBDD
 {
     public partial class Form1 : Form
     {
+        SqlConnection connectionSql;
+        string connectionString;
         public Form1()
         {
             InitializeComponent();
+            connectionString = Properties.Settings.Default.Conexion;
+            connectionSql = new SqlConnection(connectionString);
+        }
+
+        private void CargarDatosConDataReader()
+        {
+            try
+            {
+                connectionSql.Open();
+                string query = "SELECT * FROM Animal";
+                SqlCommand command = new SqlCommand(query, connectionSql);
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    // Crea una tabla en memoria con los datos de la consulta
+                    DataTable dataTable = new DataTable();
+                    dataTable.Load(reader);
+
+                    // Asigna la tabla al DataGridView
+                    dataGridView.DataSource = dataTable;
+                }
+                else
+                {
+                    MessageBox.Show("No hay datos");
+                }
+                connectionSql.Close();
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Ha ocurrido un error SQL: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ha ocurrido un error general: " + ex.Message);
+            }
+        }
+
+
+        private void CargarDatosConDataAdapter()
+        {
+            try
+            {
+                string query = "SELECT * FROM Animal";
+
+                using (SqlConnection connectionSql = new SqlConnection(connectionString))
+                {
+                    connectionSql.Open();
+
+                    using (SqlDataAdapter adapter = new SqlDataAdapter(query, connectionSql))
+                    {
+                        // Crea una tabla en memoria con los datos de la consulta
+                        DataTable dataTable = new DataTable();
+                        adapter.Fill(dataTable);
+
+                        // Asigna la tabla al DataGridView si hay datos
+                        if (dataTable.Rows.Count > 0)
+                        {
+                            dataGridView.DataSource = dataTable;
+                        }
+                        else
+                        {
+                            MessageBox.Show("No hay datos");
+                        }
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Ha ocurrido un error SQL: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ha ocurrido un error general: " + ex.Message);
+            }
+        }
+
+        private void btnCargarDatos_Click(object sender, System.EventArgs e)
+        {
+            CargarDatosConDataAdapter();
         }
     }
 }
