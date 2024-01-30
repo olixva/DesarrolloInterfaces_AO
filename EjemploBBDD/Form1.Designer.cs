c3de18789efe93ff4860afaa8852f6226ead22b9
Antonio Oliva Carceles <116766832+olixva@users.noreply.github.com>
2024-01-30 11:52:14 +0100

    Clase 30/01 Terminada

--- EjemploBBDD/Form1.Designer.cs
+++ EjemploBBDD/Form1.Designer.cs
@@ -29,48 +29,201 @@
         private void InitializeComponent()
         {
             this.dataGridView = new System.Windows.Forms.DataGridView();
-            this.btnCargarDatos = new System.Windows.Forms.Button();
+            this.btnCargarDatGrid = new System.Windows.Forms.Button();
+            this.listBoxZoos = new System.Windows.Forms.ListBox();
+            this.listBoxAnimalesZoo = new System.Windows.Forms.ListBox();
+            this.btnCargarZooListBox = new System.Windows.Forms.Button();
+            this.txtBoxInsertarZoo = new System.Windows.Forms.TextBox();
+            this.lblInsertZoo = new System.Windows.Forms.Label();
+            this.btnInsertarZoo = new System.Windows.Forms.Button();
+            this.btnBorrarZoo = new System.Windows.Forms.Button();
+            this.lblBorrarZoo = new System.Windows.Forms.Label();
+            this.btnCargarAnimalesList = new System.Windows.Forms.Button();
+            this.listBoxAnimales = new System.Windows.Forms.ListBox();
+            this.btnAsignarAnimalZoo = new System.Windows.Forms.Button();
+            this.lblAsignar = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView
             // 
             this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView.Location = new System.Drawing.Point(33, 22);
             this.dataGridView.Name = "dataGridView";
             this.dataGridView.RowHeadersWidth = 67;
             this.dataGridView.RowTemplate.Height = 30;
-            this.dataGridView.Size = new System.Drawing.Size(776, 426);
+            this.dataGridView.Size = new System.Drawing.Size(282, 269);
             this.dataGridView.TabIndex = 0;
             // 
-            // btnCargarDatos
+            // btnCargarDatGrid
             // 
-            this.btnCargarDatos.Location = new System.Drawing.Point(69, 522);
-            this.btnCargarDatos.Name = "btnCargarDatos";
-            this.btnCargarDatos.Size = new System.Drawing.Size(153, 87);
-            this.btnCargarDatos.TabIndex = 1;
-            this.btnCargarDatos.Text = "CARGAR DATOS";
-            this.btnCargarDatos.UseVisualStyleBackColor = true;
-            this.btnCargarDatos.Click += new System.EventHandler(this.btnCargarDatos_Click);
+            this.btnCargarDatGrid.Location = new System.Drawing.Point(96, 330);
+            this.btnCargarDatGrid.Name = "btnCargarDatGrid";
+            this.btnCargarDatGrid.Size = new System.Drawing.Size(153, 87);
+            this.btnCargarDatGrid.TabIndex = 1;
+            this.btnCargarDatGrid.Text = "CARGAR ANIMALES";
+            this.btnCargarDatGrid.UseVisualStyleBackColor = true;
+            this.btnCargarDatGrid.Click += new System.EventHandler(this.btnCargarDatos_Click);
+            // 
+            // listBoxZoos
+            // 
+            this.listBoxZoos.FormattingEnabled = true;
+            this.listBoxZoos.ItemHeight = 22;
+            this.listBoxZoos.Location = new System.Drawing.Point(694, 23);
+            this.listBoxZoos.Name = "listBoxZoos";
+            this.listBoxZoos.Size = new System.Drawing.Size(274, 268);
+            this.listBoxZoos.TabIndex = 2;
+            this.listBoxZoos.SelectedIndexChanged += new System.EventHandler(this.listBoxZoos_SelectedIndexChanged);
+            // 
+            // listBoxAnimalesZoo
+            // 
+            this.listBoxAnimalesZoo.FormattingEnabled = true;
+            this.listBoxAnimalesZoo.ItemHeight = 22;
+            this.listBoxAnimalesZoo.Location = new System.Drawing.Point(1011, 22);
+            this.listBoxAnimalesZoo.Name = "listBoxAnimalesZoo";
+            this.listBoxAnimalesZoo.Size = new System.Drawing.Size(277, 268);
+            this.listBoxAnimalesZoo.TabIndex = 3;
+            // 
+            // btnCargarZooListBox
+            // 
+            this.btnCargarZooListBox.Location = new System.Drawing.Point(743, 330);
+            this.btnCargarZooListBox.Name = "btnCargarZooListBox";
+            this.btnCargarZooListBox.Size = new System.Drawing.Size(153, 87);
+            this.btnCargarZooListBox.TabIndex = 4;
+            this.btnCargarZooListBox.Text = "CARGAR ZOOS";
+            this.btnCargarZooListBox.UseVisualStyleBackColor = true;
+            this.btnCargarZooListBox.Click += new System.EventHandler(this.btnCargarZooListBox_Click);
+            // 
+            // txtBoxInsertarZoo
+            // 
+            this.txtBoxInsertarZoo.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBoxInsertarZoo.Location = new System.Drawing.Point(733, 525);
+            this.txtBoxInsertarZoo.Name = "txtBoxInsertarZoo";
+            this.txtBoxInsertarZoo.Size = new System.Drawing.Size(174, 35);
+            this.txtBoxInsertarZoo.TabIndex = 5;
+            // 
+            // lblInsertZoo
+            // 
+            this.lblInsertZoo.AutoSize = true;
+            this.lblInsertZoo.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInsertZoo.Location = new System.Drawing.Point(727, 453);
+            this.lblInsertZoo.Name = "lblInsertZoo";
+            this.lblInsertZoo.Size = new System.Drawing.Size(180, 33);
+            this.lblInsertZoo.TabIndex = 6;
+            this.lblInsertZoo.Text = "Insertar Zoo:";
+            // 
+            // btnInsertarZoo
+            // 
+            this.btnInsertarZoo.Location = new System.Drawing.Point(766, 593);
+            this.btnInsertarZoo.Name = "btnInsertarZoo";
+            this.btnInsertarZoo.Size = new System.Drawing.Size(98, 56);
+            this.btnInsertarZoo.TabIndex = 7;
+            this.btnInsertarZoo.Text = "Insertar";
+            this.btnInsertarZoo.UseVisualStyleBackColor = true;
+            this.btnInsertarZoo.Click += new System.EventHandler(this.btnInsertarZoo_Click);
+            // 
+            // btnBorrarZoo
+            // 
+            this.btnBorrarZoo.Location = new System.Drawing.Point(1131, 529);
+            this.btnBorrarZoo.Name = "btnBorrarZoo";
+            this.btnBorrarZoo.Size = new System.Drawing.Size(98, 56);
+            this.btnBorrarZoo.TabIndex = 10;
+            this.btnBorrarZoo.Text = "Borrar";
+            this.btnBorrarZoo.UseVisualStyleBackColor = true;
+            this.btnBorrarZoo.Click += new System.EventHandler(this.btnBorrarZoo_Click);
+            // 
+            // lblBorrarZoo
+            // 
+            this.lblBorrarZoo.AutoSize = true;
+            this.lblBorrarZoo.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBorrarZoo.Location = new System.Drawing.Point(1014, 467);
+            this.lblBorrarZoo.Name = "lblBorrarZoo";
+            this.lblBorrarZoo.Size = new System.Drawing.Size(345, 33);
+            this.lblBorrarZoo.TabIndex = 9;
+            this.lblBorrarZoo.Text = "Borrar Zoo Seleccionado:";
+            // 
+            // btnCargarAnimalesList
+            // 
+            this.btnCargarAnimalesList.Location = new System.Drawing.Point(422, 329);
+            this.btnCargarAnimalesList.Name = "btnCargarAnimalesList";
+            this.btnCargarAnimalesList.Size = new System.Drawing.Size(153, 87);
+            this.btnCargarAnimalesList.TabIndex = 12;
+            this.btnCargarAnimalesList.Text = "CARGAR ANIMALES";
+            this.btnCargarAnimalesList.UseVisualStyleBackColor = true;
+            this.btnCargarAnimalesList.Click += new System.EventHandler(this.btnCargarAnimalesList_Click);
+            // 
+            // listBoxAnimales
+            // 
+            this.listBoxAnimales.FormattingEnabled = true;
+            this.listBoxAnimales.ItemHeight = 22;
+            this.listBoxAnimales.Location = new System.Drawing.Point(373, 22);
+            this.listBoxAnimales.Name = "listBoxAnimales";
+            this.listBoxAnimales.Size = new System.Drawing.Size(274, 268);
+            this.listBoxAnimales.TabIndex = 11;
+            // 
+            // btnAsignarAnimalZoo
+            // 
+            this.btnAsignarAnimalZoo.Location = new System.Drawing.Point(265, 578);
+            this.btnAsignarAnimalZoo.Name = "btnAsignarAnimalZoo";
+            this.btnAsignarAnimalZoo.Size = new System.Drawing.Size(121, 71);
+            this.btnAsignarAnimalZoo.TabIndex = 13;
+            this.btnAsignarAnimalZoo.Text = "ASIGNAR";
+            this.btnAsignarAnimalZoo.UseVisualStyleBackColor = true;
+            this.btnAsignarAnimalZoo.Click += new System.EventHandler(this.btnAsignarAnimalZoo_Click);
+            // 
+            // lblAsignar
+            // 
+            this.lblAsignar.AutoSize = true;
+            this.lblAsignar.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAsignar.Location = new System.Drawing.Point(36, 513);
+            this.lblAsignar.Name = "lblAsignar";
+            this.lblAsignar.Size = new System.Drawing.Size(652, 33);
+            this.lblAsignar.TabIndex = 14;
+            this.lblAsignar.Text = "Asignar Animal seleccionado a Zoo seleccionado";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 685);
-            this.Controls.Add(this.btnCargarDatos);
+            this.ClientSize = new System.Drawing.Size(1382, 706);
+            this.Controls.Add(this.lblAsignar);
+            this.Controls.Add(this.btnAsignarAnimalZoo);
+            this.Controls.Add(this.btnCargarAnimalesList);
+            this.Controls.Add(this.listBoxAnimales);
+            this.Controls.Add(this.btnBorrarZoo);
+            this.Controls.Add(this.lblBorrarZoo);
+            this.Controls.Add(this.btnInsertarZoo);
+            this.Controls.Add(this.lblInsertZoo);
+            this.Controls.Add(this.txtBoxInsertarZoo);
+            this.Controls.Add(this.btnCargarZooListBox);
+            this.Controls.Add(this.listBoxAnimalesZoo);
+            this.Controls.Add(this.listBoxZoos);
+            this.Controls.Add(this.btnCargarDatGrid);
             this.Controls.Add(this.dataGridView);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView;
-        private System.Windows.Forms.Button btnCargarDatos;
+        private System.Windows.Forms.Button btnCargarDatGrid;
+        private System.Windows.Forms.ListBox listBoxZoos;
+        private System.Windows.Forms.ListBox listBoxAnimalesZoo;
+        private System.Windows.Forms.Button btnCargarZooListBox;
+        private System.Windows.Forms.TextBox txtBoxInsertarZoo;
+        private System.Windows.Forms.Label lblInsertZoo;
+        private System.Windows.Forms.Button btnInsertarZoo;
+        private System.Windows.Forms.Button btnBorrarZoo;
+        private System.Windows.Forms.Label lblBorrarZoo;
+        private System.Windows.Forms.Button btnCargarAnimalesList;
+        private System.Windows.Forms.ListBox listBoxAnimales;
+        private System.Windows.Forms.Button btnAsignarAnimalZoo;
+        private System.Windows.Forms.Label lblAsignar;
     }
 }
 
--- EjemploBBDD/Form1.cs
+++ EjemploBBDD/Form1.cs
@@ -17,7 +17,9 @@ namespace EjemploBBDD
             connectionSql = new SqlConnection(connectionString);
         }
 
-        private void CargarDatosConDataReader()
+        //Cargar datos en el DataGridView con un DataReader 
+        //Util si solo se va a leer los datos una vez
+        private void CargarAnimalesDataGridConDataReader()
         {
             try
             {
@@ -53,7 +55,9 @@ namespace EjemploBBDD
         }
 
 
-        private void CargarDatosConDataAdapter()
+        //Cargar datos en el DataGridView con un DataAdapter
+        //Util si se va a leer los datos varias veces o realizar operaciones en los datos
+        private void CargarAnimalesDataGridConDataAdapter()
         {
             try
             {
@@ -93,7 +97,285 @@ namespace EjemploBBDD
 
         private void btnCargarDatos_Click(object sender, System.EventArgs e)
         {
-            CargarDatosConDataAdapter();
+            CargarAnimalesDataGridConDataAdapter();
+        }
+
+        //Cargar datos en el ListBox de Zoos con un DataAdapter
+        private void CargarDatosZooListBox()
+        {
+            try
+            {
+                string query = "SELECT * FROM Zoo";
+
+                using (SqlConnection connectionSql = new SqlConnection(connectionString))
+                {
+                    connectionSql.Open();
+
+                    using (SqlDataAdapter adapter = new SqlDataAdapter(query, connectionSql))
+                    {
+                        // Crea una tabla en memoria con los datos de la consulta
+                        DataTable dataTable = new DataTable();
+                        adapter.Fill(dataTable);
+
+                        // Asigna la tabla al ListBox si hay datos
+                        if (dataTable.Rows.Count > 0)
+                        {
+                            listBoxZoos.DisplayMember = "Ubicacion";
+                            listBoxZoos.ValueMember = "Id";
+                            listBoxZoos.DataSource = dataTable;
+                        }
+                        else
+                        {
+                            MessageBox.Show("No hay datos");
+                        }
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Ha ocurrido un error SQL: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ha ocurrido un error general: " + ex.Message);
+            }
+        }
+
+        private void btnCargarZooListBox_Click(object sender, EventArgs e)
+        {
+            CargarDatosZooListBox();
+        }
+
+        //Cargar datos en el ListBox de Animales con un DataAdapter
+
+        private void CargarAnimalesListBox()
+        {
+            try
+            {
+                string query = "SELECT * FROM Animal";
+
+                using (SqlConnection connectionSql = new SqlConnection(connectionString))
+                {
+                    connectionSql.Open();
+
+                    using (SqlDataAdapter adapter = new SqlDataAdapter(query, connectionSql))
+                    {
+                        // Crea una tabla en memoria con los datos de la consulta
+                        DataTable dataTable = new DataTable();
+                        adapter.Fill(dataTable);
+
+                        // Asigna la tabla al ListBox si hay datos
+                        if (dataTable.Rows.Count > 0)
+                        {
+                            listBoxAnimales.DisplayMember = "Nombre";
+                            listBoxAnimales.ValueMember = "Id";
+                            listBoxAnimales.DataSource = dataTable;
+                        }
+                        else
+                        {
+                            MessageBox.Show("No hay datos");
+                        }
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Ha ocurrido un error SQL: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ha ocurrido un error general: " + ex.Message);
+            }
+        }
+
+
+        private void btnCargarAnimalesList_Click(object sender, EventArgs e)
+        {
+            CargarAnimalesListBox();
+        }
+
+        //Cargar datos en el ListBox de AnimalesZoo con un DataAdapter
+        private void CargarAnimalesPorZooListBox()
+        {
+            try
+            {
+                string query = "SELECT * FROM Animal a INNER JOIN AnimalZoo az ON a.Id=az.animalId WHERE az.zooId=@zooId";
+
+                using (SqlConnection connectionSql = new SqlConnection(connectionString))
+                {
+                    connectionSql.Open();
+                    SqlCommand command = new SqlCommand(query, connectionSql);
+                    command.Parameters.AddWithValue("@zooId", listBoxZoos.SelectedValue);
+
+                    using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+                    {
+                        // Crea una tabla en memoria con los datos de la consulta
+                        DataTable dataTable = new DataTable();
+                        adapter.Fill(dataTable);
+
+                        // Asigna la tabla al DataGridView si hay datos
+                        if (dataTable.Rows.Count > 0)
+                        {
+                            listBoxAnimalesZoo.DisplayMember = "Nombre";
+                            listBoxAnimalesZoo.ValueMember = "Id";
+                            listBoxAnimalesZoo.DataSource = dataTable;
+                        }
+                        else
+                        {
+                            MessageBox.Show("No hay datos");
+                        }
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Ha ocurrido un error SQL: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ha ocurrido un error general: " + ex.Message);
+            }
+        }
+
+        private void listBoxZoos_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            CargarAnimalesPorZooListBox();
+        }
+
+        //Cargar datos en la tabla de Zoo ejecutando directamente el SqlCommand
+        private int AgregarZoo(String nombre)
+        {
+            try
+            {
+                string query = "INSERT INTO Zoo VALUES (@Ubicacion)";
+
+                using (SqlConnection connectionSql = new SqlConnection(connectionString))
+                {
+                    connectionSql.Open();
+                    SqlCommand command = new SqlCommand(query, connectionSql);
+                    command.Parameters.AddWithValue("@Ubicacion", nombre);
+
+                    return command.ExecuteNonQuery();
+                }
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Ha ocurrido un error SQL: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ha ocurrido un error general: " + ex.Message);
+            }
+            return 0;
+        }
+
+        private void btnInsertarZoo_Click(object sender, EventArgs e)
+        {
+            if (txtBoxInsertarZoo.Text == "")
+            {
+                MessageBox.Show("Debe ingresar un nombre de zoo");
+            }
+            else
+            {
+                AgregarZoo(txtBoxInsertarZoo.Text);
+                CargarDatosZooListBox();
+                txtBoxInsertarZoo.Text = "";
+            }
+        }
+
+        //Borrar datos en la tabla de Zoo ejecutando directamente el SqlCommand
+        private int BorrarZoo(int id)
+        {
+            try
+            {
+                string query = "DELETE FROM Zoo WHERE id = @ZooId";
+
+                using (SqlConnection connectionSql = new SqlConnection(connectionString))
+                {
+                    connectionSql.Open();
+                    SqlCommand command = new SqlCommand(query, connectionSql);
+                    command.Parameters.AddWithValue("@ZooId", id);
+
+                    return command.ExecuteNonQuery();
+                }
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Ha ocurrido un error SQL: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ha ocurrido un error general: " + ex.Message);
+            }
+            return 0;
+        }
+
+        private void btnBorrarZoo_Click(object sender, EventArgs e)
+        {
+            if (listBoxZoos.SelectedIndex == -1)
+            {
+                MessageBox.Show("Debe seleccionar un zoo");
+            }
+            else
+            {
+                if (BorrarZoo((int)listBoxZoos.SelectedValue) != 0)
+                {
+                    CargarDatosZooListBox();
+                }
+                else
+                {
+                    MessageBox.Show("No se ha podido borrar el zoo");
+                }
+            }
+        }
+
+        //Cargar datos en la tabla de AnimalZoo ejecutando directamente el SqlCommand
+        private int AgregarAnimalZoo(int zooId, int animalId)
+        {
+            try
+            {
+                string query = "INSERT INTO AnimalZoo VALUES (@ZooId, @AnimalId)";
+
+                using (SqlConnection connectionSql = new SqlConnection(connectionString))
+                {
+                    connectionSql.Open();
+                    SqlCommand command = new SqlCommand(query, connectionSql);
+                    command.Parameters.AddWithValue("@ZooId", zooId);
+                    command.Parameters.AddWithValue("@AnimalId", animalId);
+
+                    return command.ExecuteNonQuery();
+                }
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Ha ocurrido un error SQL: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ha ocurrido un error general: " + ex.Message);
+            }
+            return 0;
+        }
+
+        private void btnAsignarAnimalZoo_Click(object sender, EventArgs e)
+        {
+            if (listBoxZoos.SelectedIndex == -1 || listBoxAnimales.SelectedIndex == -1)
+            {
+                MessageBox.Show("Debe seleccionar un Animal y un Zoo");
+            }
+            else
+            {
+                if (AgregarAnimalZoo((int)listBoxZoos.SelectedValue, (int)listBoxAnimales.SelectedValue) != 0)
+                {
+                    CargarAnimalesPorZooListBox();
+                }
+                else
+                {
+                    MessageBox.Show("No se ha podido borrar el zoo");
+                }
+            }
         }
     }
 }
+
+
